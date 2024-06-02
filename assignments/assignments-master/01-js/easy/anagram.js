1491fbfc85a5f2f7b9bbb97aed5034c5290e3fac
rupinderjdn <82199133+rupinderjdn@users.noreply.github.com>
2024-06-02 18:28:45 +0530

    @Cohort2 @assignment setup

---
+++ assignments/assignments-master/01-js/easy/anagram.js
@@ -0,0 +1,26 @@
+/*
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+*/
+
+function isAnagram(str1, str2) {
+  let hashmap = {};
+  str1= str1.toLowerCase();
+  str2= str2.toLowerCase();
+  if(str1 === str2)return true;
+  for(let character of str1){
+    if(hashmap[character]==undefined)hashmap[character] = 1;
+    else hashmap[character] +=1;
+  }
+  for(let character of str2){
+    if(hashmap[character]==undefined)hashmap[character] = -1;
+    else hashmap[character] -=1;
+  }
+  for(let key in hashmap){
+    if(hashmap[key] !== 0)return false;
+  }
+  console.log(str1);
+  return true;
+}
+module.exports = isAnagram;
