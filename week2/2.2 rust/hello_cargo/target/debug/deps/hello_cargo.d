b0f69249a48ddcc2e6e854994689e3e083331958
rupinderjdn <82199133+rupinderjdn@users.noreply.github.com>
2024-06-28 18:21:41 +0530

    @cohort2 @week2.2 @cargo intro in rust

---
+++ week2/2.2 rust/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+D:\CodeProjects\Cohort2\week2\2.2 rust\hello_cargo\target\debug\deps\hello_cargo.exe: src/main.rs
+
+D:\CodeProjects\Cohort2\week2\2.2 rust\hello_cargo\target\debug\deps\hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ week2/2.2 rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\CodeProjects\Cohort2\week2\2.2\ rust\hello_cargo\target\debug\hello_cargo.exe: D:\CodeProjects\Cohort2\week2\2.2\ rust\hello_cargo\src\main.rs
