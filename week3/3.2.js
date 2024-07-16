90aca89414a764f1c9ab1e58c27d5e6b6621b272
rupinderjdn <82199133+rupinderjdn@users.noreply.github.com>
2024-07-16 21:29:54 +0530

    @cohort2 @week3.2 @token recieve from the signin request

--- week3/3.2.js
+++ week3/3.2.js
@@ -24,3 +24,76 @@ function getPersonData(){
 // --------- AUTHENTICATION ------------------------
 // Hashing => 1 way
 // Encryption => 2 way with a key
+
+
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const jwtPassword = "123456";
+
+const app=express();
+app.use(express.json())
+// in memory users
+const ALL_USERS = [
+  {
+    username: "rupinder@gmail.com",
+    password: "123",
+    name: "rupinder singh"
+  },
+  {
+    username: "rupinder2@gmail.com",
+    password: "123",
+    name: "rupinder2 singh"
+  },
+  {
+    username: "rupinder3@gmail.com",
+    password: "123",
+    name: "rupinder3 singh"
+  },
+]
+
+function userExists(username,password){
+  // verifying if the user exists or not
+  let result =false;
+  ALL_USERS.forEach((userEntry)=>{
+    // console.log(username,password);
+    // console.log(userEntry);
+    if(userEntry.username === username && userEntry.password === password){
+      result = true;
+    }
+  })
+  return result;
+}
+
+app.post("/signin",(req,res)=>{
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if(!userExists(username,password)){
+    return res.status(403).json({
+      msg: "user doesnt exist in our in memory db"
+    })
+  }
+
+  // what is this shhh? jwtpassword 
+  var token = jwt.sign({username : username}, jwtPassword);
+  return res.json({
+    token,
+  })
+})
+
+
+// this request expects a authorization header from the user
+app.get("/users",(req,res)=>{
+  const token = req.headers.authorization;
+  try{
+    const decoded = jwt.verify(token, jwtPassword);
+    const username = decoded.username;
+  }
+  catch(err){
+    return res.status(403).json({
+      msg: "Invalid token",
+    })
+  }
+})
+
+app.listen(3000);
